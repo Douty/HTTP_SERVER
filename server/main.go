f0eb0f069a4cda6f3eefab22bb6710eac3eb77ba
Douty <108250979+Douty@users.noreply.github.com>
2026-01-14 05:01:31 -0500

    created handle connection function for tcp socket server

--- server/main.go
+++ server/main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net"
 )
 
-func handleConnection() {
-	socket, err := net.Listen(tcp, ":8000")
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+	bytesLength, err := conn.Read(buffer)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Println(buffer[:bytesLength])
 }
 
 func main() {
-
+	socket, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Listening on port 8000")
+	for {
+		connections, err := socket.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleConnection(connections)
+	}
 }
