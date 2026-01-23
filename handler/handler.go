8810a164440c1d8cac544d33ae83bc8262f07eac
Douty <108250979+Douty@users.noreply.github.com>
2026-01-23 01:43:41 -0500

    - Reorganized files - Added query parsing in GET requests

---
+++ handler/handler.go
@@ -0,0 +1,80 @@
+package handler
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
+
+type Method string
+
+const (
+	GET     Method = "GET"
+	POST    Method = "POST"
+	PUT     Method = "PUT"
+	HEAD    Method = "HEAD"
+	OPTIONS Method = "OPTIONS"
+	CONNECT Method = "CONNECT"
+	DELETE  Method = "DELETE"
+	PATCH   Method = "PATCH"
+	TRACE   Method = "TRACE"
+)
+
+type Request struct {
+	Method  Method
+	route   string
+	version string
+	headers map[string]string
+	query   map[string]string
+}
+
+func ParseRequest(conn net.Conn) (Request, error) {
+	var request Request
+
+	request.headers = make(map[string]string)
+	request.query = make(map[string]string)
+
+	broswerRequest := bufio.NewReader(conn)
+
+	//collects the the method, route and version sent
+	requestLine, err := broswerRequest.ReadString('\n')
+	if err != nil {
+		return request, fmt.Errorf("Failed to properly read %w", err)
+	}
+
+	parts := strings.Split(requestLine, " ")
+	if len(parts) != 3 {
+		return request, fmt.Errorf("Invalid request recieved")
+	}
+	request.Method = Method(parts[0])
+	request.version = parts[2]
+
+	// collecting the route and queries
+
+	fullroute := parts[1]
+
+	routepart := strings.SplitN(fullroute, "?", 2)
+	request.route = routepart[0]
+
+	if len(routepart) == 2 {
+		if request.Method == GET {
+			queries := routepart[1]
+			fields := strings.Split(queries, "&")
+			for _, field := range fields {
+				pair := strings.SplitN(field, "=", 2)
+				if len(pair) == 2 {
+					key := pair[0]
+					value := pair[1]
+					request.query[key] = value
+				} else if len(pair) == 1 {
+					request.query[pair[0]] = ""
+				}
+
+			}
+
+		}
+	}
+
+	return request, nil
+}
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"httpserver/handler"
+	"log"
+	"net"
+	"os"
+)
+
+func readHTMLFile(filepath string) string {
+	var HTML string
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		HTML += line
+	}
+	return HTML
+}
+
+// func generateHttpGetResponse() string {
+// 	htmlFile := readHTMLFile("index.html")
+// 	httpResponse := "HTTP/1.1 200 OK\r\n"
+// 	httpResponse += "date: " + time.Now().Format(time.RFC1123) + "\r\n"
+// 	httpResponse += "Server: " + "Custom HTTP server\r\n"
+// 	httpResponse += "content-type: " + "text/html\r\n"
+// 	httpResponse += "content-length: " + strconv.Itoa(len(htmlFile)) + "\r\n"
+// 	httpResponse += "\r\n"
+// 	httpResponse += htmlFile
+
+// 	return httpResponse
+// }
+// func handleHttpPostResponse() string {
+// 	httpResponse := "HTTP/1.1 201 Created\r\n"
+// 	httpResponse += "date: " + time.Now().Format(time.RFC1123) + "\r\n"
+// 	httpResponse += "Server: " + "Custom HTTP server\r\n"
+// 	httpResponse += "content-type: " + "text/html\r\n"
+
+// 	return httpResponse
+// }
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	response, err := handler.ParseRequest(conn)
+	if err != nil {
+		fmt.Print("Error Occured: %w", err)
+	}
+	fmt.Printf("%v", response)
+}
+
+func main() {
+	socket, err := net.Listen("tcp", ":80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Listening on port 80")
+	for {
+		connections, err := socket.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleConnection(connections)
+	}
+}
